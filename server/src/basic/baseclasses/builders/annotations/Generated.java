4beb2e10cc7fc862d42b7335c30b08bf4b7836e0
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2023-06-14 01:09:49 +0300

    annotations

--- server/src/basic/baseclasses/builders/annotations/Generated.java
+++ server/src/basic/baseclasses/builders/annotations/Generated.java
@@ -1,4 +1,11 @@
 package basic.baseclasses.builders.annotations;
 
-public class Generated {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Generated {
 }

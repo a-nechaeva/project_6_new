ef90a302e77f039e7e8adeda80198208d284d55e
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2023-06-14 01:08:58 +0300

    annotations

--- server/src/basic/baseclasses/builders/annotations/NotEmpty.java
+++ server/src/basic/baseclasses/builders/annotations/NotEmpty.java
@@ -1,4 +1,14 @@
 package basic.baseclasses.builders.annotations;
 
-public class NotEmpty {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation in used to indicate that String field can't be empty
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface NotEmpty {
 }

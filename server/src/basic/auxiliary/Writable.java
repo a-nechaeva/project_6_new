d62a27bb1fed342017342e301f5bf33299360fd0
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2023-06-13 23:06:10 +0300

    server_in_progress

---
+++ server/src/basic/auxiliary/Writable.java
@@ -0,0 +1,7 @@
+package basic.auxiliary;
+
+public interface Writable {
+    String takeEntry();
+
+    String takeEntryName();
+}
---
+++ server/src/basic/baseclasses/Studio.java
@@ -0,0 +1,4 @@
+package basic.baseclasses;
+
+public class Studio {
+}
